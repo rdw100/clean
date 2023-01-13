1946e54129247c3cc4761187dcb2b4482ff0b619
rdw100 <ryandustinwright@hotmail.com>
2023-01-13 14:13:24 -0500

    Added repository interface

--- mn/bank/Bank.Domain/Interface/IAccountRepository.cs
+++ mn/bank/Bank.Domain/Interface/IAccountRepository.cs
@@ -1,9 +1,4 @@
 ﻿using Bank.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bank.Domain.Interface
 {
