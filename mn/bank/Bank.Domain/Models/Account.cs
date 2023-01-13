3ebb4e45131a791fc7a49e82f350b9f86ffca19c
rdw100 <ryandustinwright@hotmail.com>
2023-01-12 19:50:05 -0500

    Added account domain model

---
+++ mn/bank/Bank.Domain/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Domain.Models
+{
+    public class Account
+    {
+        public int Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public Account(string name, decimal initialBalance)
+        {
+            Owner = name;
+            Balance = initialBalance;
+        }
+    }
+}
