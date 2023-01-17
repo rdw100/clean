55d4d7fa36791fb67b017e62a94d6773f57ade34
rdw100 <ryandustinwright@hotmail.com>
2023-01-17 16:30:21 -0500

    Added web api.

---
+++ mn/bank/Bank.Api/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+
+    }
+}
