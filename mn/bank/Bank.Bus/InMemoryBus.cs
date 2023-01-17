5e183cc7c16dea5f58818f8921e6073f28d9fc34
rdw100 <ryandustinwright@hotmail.com>
2023-01-17 09:41:31 -0500

    Adding domain core.  Added in-memory bus.

--- mn/bank/Bank.Bus/InMemoryBus.cs
+++ mn/bank/Bank.Bus/InMemoryBus.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bank.Core.Bus;
+using Bank.Core.Commands;
+using MediatR;
 
 namespace Bank.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command 
+        {
+            return _mediator.Send(command);
+        }
     }
 }
---
+++ mn/bank/Bank.Core/Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using Bank.Core.Events;
+
+namespace Bank.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
