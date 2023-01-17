2354acac4e406d9a65ae443ec23de7f5b6482ce3
rdw100 <ryandustinwright@hotmail.com>
2023-01-17 16:37:45 -0500

    Added IoC bootstrap, connection, context, and appsettings to the Api

--- mn/bank/Bank.Api/Program.cs
+++ mn/bank/Bank.Api/Program.cs
@@ -1,11 +1,22 @@
+using Bank.Data.Context;
+using Bank.IoC;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var bankConnectionString = builder.Configuration.GetConnectionString("BankDbConnection") ?? throw new InvalidOperationException("Connection string 'BankDbConnection' not found.");
+builder.Services.AddDbContext<BankDbContext>(options =>
+{
+    options.UseSqlServer(bankConnectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+RegisterServices(builder.Services);
 
 var app = builder.Build();
 
@@ -23,3 +34,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void RegisterServices(IServiceCollection services)
+{
+    DependencyContainer.RegisterServices(services);
+}
