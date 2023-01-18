b12b2f13aac1b0f395937091733d44232610815c
rdw100 <ryandustinwright@hotmail.com>
2023-01-18 09:06:09 -0500

    Resolve merge conflict accept feature branch.  Take incoming.  55d4d7fa.

--- mn/bank/Bank.Api/Program.cs
+++ mn/bank/Bank.Api/Program.cs
@@ -1,11 +1,26 @@
+using Bank.Data.Context;
+using Bank.IoC;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var bankConnectionString = builder.Configuration.GetConnectionString("BankDbConnection") ?? throw new InvalidOperationException("Connection string 'BankDbConnection' not found.");
+builder.Services.AddDbContext<BankDbContext>(options =>
+{
+    options.UseSqlServer(bankConnectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+RegisterServices(builder.Services);
 
 var app = builder.Build();
 
@@ -23,3 +38,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void RegisterServices(IServiceCollection services)
+{
+    DependencyContainer.RegisterServices(services);
+}
