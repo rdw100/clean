e560b884991d3fd331fee911a97369d11a053e5b
rdw100 <ryandustinwright@hotmail.com>
2023-01-21 16:06:50 -0500

    Added improved CleanLayer testing.

---
+++ mn/bank/Bank.Tests.Architecture/CleanLayerTest.cs
@@ -0,0 +1,108 @@
+﻿using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Bank.Tests.Arch
+{
+    public class CleanLayerTest
+    {
+        private static readonly Architecture domainCoreArchitecture =
+            new ArchLoader().LoadAssemblies(
+                    System.Reflection.Assembly.Load("Bank.Domain")
+                )
+                .Build();
+
+        private static readonly Architecture domainToCoreArchitecture =
+            new ArchLoader().LoadAssemblies(
+                    System.Reflection.Assembly.Load("Bank.Domain"),
+                    System.Reflection.Assembly.Load("Bank.Domain.Core")
+                )
+                .Build();
+
+        private static readonly Architecture domainToAppArchitecture =
+            new ArchLoader().LoadAssemblies(
+                    System.Reflection.Assembly.Load("Bank.Domain"),
+                    System.Reflection.Assembly.Load("Bank.Application")
+                )
+                .Build();
+
+        private static readonly Architecture applicationToPresentationArchitecture =
+            new ArchLoader().LoadAssemblies(
+                    System.Reflection.Assembly.Load("Bank.Application"),
+                    System.Reflection.Assembly.Load("Bank.Mvc")
+                )
+                .Build();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>HaveFullNameContaining traverses qualified identifiers.</remarks>
+        private readonly IObjectProvider<IType> DomainLayer =
+            Types().That().HaveFullNameContaining("Bank.Domain").As("Domain Layer");
+
+        private readonly IObjectProvider<IType> DomainCoreLayer =
+            Types().That().HaveFullNameContaining("Bank.Domain.Core").As("Domain Core Layer");
+
+        private readonly IObjectProvider<IType> ApplicationLayer =
+            Types().That().HaveFullNameContaining("Bank.Application").As("Application Layer");
+
+        private readonly IObjectProvider<IType> PresentationLayer =
+            Types().That().HaveFullNameContaining("Bank.Mvc").As("Presentation Layer");
+
+        //Types().That().ResideInNamespace("Bank.Domain.Core").As("Forbidden Layer");
+
+        //private readonly IObjectProvider<IType> Domain =
+        //    Types().That().ResideInAssembly("Bank.Domain").As("Domain Layer");
+
+        //private readonly IObjectProvider<IType> DomainCore =
+        //    Types().That().ResideInAssembly("Bank.Domain.Core").As("Domain Layer - Core");
+
+        //private readonly IObjectProvider<IType> Application =
+        //    Types().That().ResideInAssembly("Bank.Application").As("Application Layer");
+
+        //private readonly IObjectProvider<IType> Presentation =
+        //    Types().That().ResideInAssembly("Bank.Mvc").As("Presentation Layer");
+
+        [Fact]
+        public void ApplicationLayer_DoesNotDependOnPresentation_ReturnsTrue()
+        {
+            IArchRule applicationLayerShouldNotAccessPresentationLayer =
+                Types().That().Are(ApplicationLayer).Should()
+                .NotDependOnAny(PresentationLayer).Because("It's required.");
+            bool checkedRule = applicationLayerShouldNotAccessPresentationLayer.HasNoViolations(applicationToPresentationArchitecture);
+            Assert.True(checkedRule, "Domain must only depend on Domain Core.");
+            //exampleLayerShouldNotAccessForbiddenLayer.Check(applicationToPresentationArchitecture);
+        }
+
+        [Fact]
+        public void ApplicationLayer_DoesNotDependOnPresentation_ReturnsFalse()
+        {
+            IArchRule applicationLayerShouldNotAccessPresentationLayer =
+                Types().That().Are(ApplicationLayer).Should()
+                .DependOnAny(PresentationLayer).Because("It's forbidden.");
+            bool checkedRule = applicationLayerShouldNotAccessPresentationLayer.HasNoViolations(applicationToPresentationArchitecture);
+            Assert.False(checkedRule, "Domain must only depend on Domain Core.");
+            //exampleLayerShouldNotAccessForbiddenLayer.Check(applicationToPresentationArchitecture);
+        }
+
+        [Fact]
+        public void DomainLayer_DoesNotDependOnApplicationLayer_ReturnsTrue()
+        {
+            IArchRule domainLayerShouldNotAccessApplicationLayer =
+                Types().That().Are(DomainLayer).Should()
+                .NotDependOnAny(ApplicationLayer).Because("It's required.");            
+            bool checkedRule = domainLayerShouldNotAccessApplicationLayer.HasNoViolations(domainToAppArchitecture);
+            Assert.True(checkedRule, "Domain must not access Application.");
+            //domainLayerShouldNotAccessApplicationLayer.Check(domainToAppArchitecture);
+        }
+
+        [Fact]
+        public void DomainLayer_DoesNotDependOnApplicationLayer_ReturnsFalse()
+        {
+            IArchRule domainLayerShouldNotAccessApplicationLayer =
+                Types().That().Are(DomainLayer).Should()
+                .DependOnAny(ApplicationLayer).Because("It's forbidden.");
+            bool checkedRule = domainLayerShouldNotAccessApplicationLayer.HasNoViolations(domainToAppArchitecture);
+            Assert.False(checkedRule, "Domain must not access Application.");
+            //domainLayerShouldNotAccessApplicationLayer.Check(domainToAppArchitecture);
+        }
+    }
+}
--- mn/bank/Bank.Tests.Architecture/HierarchicalSystemTests.cs
+++ mn/bank/Bank.Tests.Architecture/HierarchicalSystemTests.cs
@@ -1,5 +1,4 @@
-﻿using ArchUnitNET.Domain;
-using ArchUnitNET.xUnit;
+﻿using ArchUnitNET.xUnit;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
 namespace Bank.Tests.Arch
@@ -13,37 +12,72 @@ namespace Bank.Tests.Arch
         //private const string PresentationNamespace = SystemNamespace + ".Ui";
 
         //TIP: load your architecture once at the start to maximize performance of your tests
-        private static readonly Architecture architectureDomain = new ArchLoader().LoadAssemblies(
+        private static readonly Architecture architectureDomain =
+            new ArchLoader().LoadAssemblies(
                 System.Reflection.Assembly.Load("Bank.Domain.Core"),
-                System.Reflection.Assembly.Load("Bank.Domain"))
-            .Build();
+                System.Reflection.Assembly.Load("Bank.Domain")
+            ).Build();
+
+        [Fact]
+        public void DomainShouldNotAccessDomainCore()
+        {
+            IArchRule domainShouldNotAccessCore =
+                Types().That().Are(Application).Should()
+                .NotDependOnAny(Presentation).Because("it's forbidden");
+                
+            domainShouldNotAccessCore.Check(architectureDomain);
+        }
+
+
         // replace <ExampleClass> and <ForbiddenClass> with classes from the assemblies you want to test
 
+        private static readonly Architecture architectureApplication = new ArchLoader().LoadAssemblies(
+            System.Reflection.Assembly.Load("Bank.Application"))
+            .Build();
+        
+        private static readonly Architecture architectureInfrastructureBus = new ArchLoader().LoadAssemblies(
+            System.Reflection.Assembly.Load("Bank.Infra.Bus"))
+            .Build();
+
+        private static readonly Architecture architectureInfrastructureData = new ArchLoader().LoadAssemblies(
+            System.Reflection.Assembly.Load("Bank.Infra.Data"))
+            .Build();
+
+        private static readonly Architecture architectureInfrastructureIoC = new ArchLoader().LoadAssemblies(
+            System.Reflection.Assembly.Load("Bank.Infra.IoC"))
+            .Build();
+        
         private readonly IObjectProvider<IType> Domain =
             Types().That().ResideInAssembly("Bank.Domain").As("Domain Layer");
 
         private readonly IObjectProvider<IType> DomainCore =
             Types().That().ResideInAssembly("Bank.Domain.Core").As("Domain Layer - Core");
 
-        [Fact]
-        public void DomainCoreDependsOnNothingTest()
-        {
-            IArchRule rule = Types().That().ResideInNamespace("Bank.Domain.Core").Should()
-                    .NotDependOnAny(Types().That().ResideInNamespace("Bank.Domain"));
-
-            bool checkedRule = rule.HasNoViolations(architectureDomain);
-            Assert.True(checkedRule, "Domain Core must not reference any other projects.");
-        }
-
-        private static readonly Architecture architectureApplication = new ArchLoader().LoadAssemblies(
-        System.Reflection.Assembly.Load("Bank.Application"))
-            .Build();
-
         private readonly IObjectProvider<IType> Application =
             Types().That().ResideInAssembly("Bank.Application").As("Application Layer");
 
         private readonly IObjectProvider<IType> Presentation =
             Types().That().ResideInAssembly("Bank.Mvc").As("Presentation Layer");
+
+            //IObjectProvider<IType>[] moreTypes = new[]
+            //{
+            //    ApplicationLayer,
+            //    DomainLayer
+            //};
+
+        [Fact]
+        public void Domain_OnlyDependsOnCore_Test()       
+        {
+            IArchRule rule = Types()
+                .That()
+                .ResideInNamespace("Bank.Domain")
+                .Should()
+                .OnlyDependOnTypesThat()
+                .ResideInAssembly("Bank.Application");
+                
+            bool checkedRule = rule.HasNoViolations(architectureDomain);
+            Assert.True(checkedRule, "Domain must only depend on Domain Core.");
+        }
 
         [Fact]
         public void ApplicationShouldNotAccessPresentation()
@@ -108,17 +142,5 @@ namespace Bank.Tests.Arch
 
             domainShouldNotAccessOtherLayers.Check(architectureApplication);
         }
-
-        private static readonly Architecture architectureInfrastructureBus = new ArchLoader().LoadAssemblies(
-                System.Reflection.Assembly.Load("Bank.Infra.Bus"))
-            .Build();
-
-        private static readonly Architecture architectureInfrastructureData = new ArchLoader().LoadAssemblies(
-                System.Reflection.Assembly.Load("Bank.Infra.Data"))
-            .Build();
-
-        private static readonly Architecture architectureInfrastructureIoC = new ArchLoader().LoadAssemblies(
-                System.Reflection.Assembly.Load("Bank.Infra.IoC"))
-            .Build();
     }
 }
