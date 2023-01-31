2d4eb25c3445b359e8a6ca101bd04442e578ddfb
rdw100 <ryandustinwright@hotmail.com>
2023-01-31 16:03:07 -0500

    Added domain and classes.  Added base entity for repeated attributes.

---
+++ tw/leave/Leave.Domain/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using Leave.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leave.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }    
+        public int Period { get; set; }
+       
+    }
+}
