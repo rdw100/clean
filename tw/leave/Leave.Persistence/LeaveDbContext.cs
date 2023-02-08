a39515695c6282badb675020e5bf8ddaafb60fa4
rdw100 <ryandustinwright@hotmail.com>
2023-02-08 11:27:07 -0500

    Added EF Core context

---
+++ tw/leave/Leave.Persistence/LeaveDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Leave.Domain;
+using Leave.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leave.Persistence
+{
+    public class LeaveDbContext : DbContext
+    {
+        public LeaveDbContext(DbContextOptions<LeaveDbContext> options) 
+            : base (options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate= DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
