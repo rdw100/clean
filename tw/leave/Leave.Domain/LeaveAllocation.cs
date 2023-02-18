b42ba56e31f9a63bee384aa4d66d36e1533d2552
rdw100 <ryandustinwright@hotmail.com>
2023-02-18 17:45:17 -0500

    Added employee id to allocation

--- tw/leave/Leave.Domain/LeaveAllocation.cs
+++ tw/leave/Leave.Domain/LeaveAllocation.cs
@@ -7,6 +7,7 @@ namespace Leave.Domain
         public int NumberOfDays { get; set; }
         public LeaveType LeaveType { get; set; }
         public int LeaveTypeId { get; set; }    
-        public int Period { get; set; }       
+        public int Period { get; set; }
+        public string EmployeeId { get; set; }
     }
 }
